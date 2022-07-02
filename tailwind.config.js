ac5b39b0695212476cd8d1b96a65e7992b0a812d
gladwinch <gladwinch20@gmail.com>
2022-07-02 15:23:21 +0530

    theme mode added

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = { 
+	darkMode: 'class',
+	theme: {
+		darkSelector: '.dark-mode',
+        colors: {
+			primary: {
+				'light': '#fdf0d5',
+				'dark': '#3a3335'
+			}
+		}
+    },
+	variants: {
+        backgroundColor: ['dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
+		borderColor: ['dark', 'dark-disabled', 'dark-focus', 'dark-focus-within'],
+		textColor: ['dark', 'dark-hover', 'dark-active', 'dark-placeholder']
+    },
+	plugins: [require('tailwindcss-dark-mode')()],
+	content: [
+		'components/**/*.vue',
+		'layouts/**/*.vue',
+		'pages/**/*.vue',
+		'plugins/**/*.js',
+		'nuxt.config.js'
+	],
+	tailwindcss: {
+		cssPath: '~/assets/css/tailwind.css',
+		configPath: 'tailwind.config.js',
+		exposeConfig: false,
+		config: {},
+		injectPosition: 0,
+		viewer: true,
+	},
+	colorMode: {
+		preference: 'system',
+		fallback: 'light',
+		hid: 'nuxt-color-mode-script',
+		globalName: '__NUXT_COLOR_MODE__',
+		componentName: 'ColorScheme'
+	} 
+}
